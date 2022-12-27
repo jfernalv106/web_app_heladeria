943a1b0f76540a6f8225eddb724b0088f8a791a9
joaquin fernandez <47857926+jfernalv106@users.noreply.github.com>
2022-12-27 14:13:55 -0300

    caja

--- src/app/caja/venta/venta.component.ts
+++ src/app/caja/venta/venta.component.ts
@@ -11,7 +11,8 @@ export class VentaComponent implements OnInit {
     total: number = 0;
     iva: number = 0;
     neto: number = 0;
-    producto:Producto={};
+    descuento:number=0;
+    producto: Producto = {};
 
     carrito: Producto[] = [];
     sugerencias: Producto[] = [];
@@ -24,16 +25,15 @@ export class VentaComponent implements OnInit {
         console.log('Venta');
     }
 
-     agregar(prod:Producto) {
-
-      console.log(prod);
-      console.log('La Wea');
+    agregar(prod: Producto) {
+        console.log(prod);
+        console.log('La Wea');
         this.carrito.push(prod);
         this.calcular();
     }
     quitar(pr: Producto) {
-      this.carrito=this.carrito.filter((i) => i !== pr);
-      this.calcular();
+        this.carrito = this.carrito.filter((i) => i !== pr);
+        this.calcular();
     }
     calcular() {
         this.total = 0;
@@ -41,29 +41,27 @@ export class VentaComponent implements OnInit {
         this.neto = 0;
         for (const compra of this.carrito) {
             this.total += parseInt(compra.precio ?? '0');
-          }
-          this.iva = parseInt(`${this.total * 0.19}`);
-          this.neto = parseInt(`${this.total * 0.89}`);
-          console.log(this.total);
+        }
+        this.iva = parseInt(`${this.total * 0.19}`);
+        this.neto = parseInt(`${this.total * 0.89}`);
+        console.log(this.total);
     }
 
-    autoComplete(event:any) {
-      //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
-      let filtered : Producto[] = [];
-      let query = event.query;
-      this.inventarioService.buscarProducto(query, true, '', 0, 0).subscribe(
-        (prod) => {
-          filtered = prod.productos ?? [];
-          this.sugerencias = filtered;
-            
-            
-          },
-          (err) => {
-           // filtered = [];
-           
-        }
-    );
-   
-
-  }
+    autoComplete(event: any) {
+        //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
+        let filtered: Producto[] = [];
+        let query = event.query;
+        this.inventarioService.buscarProducto(query, true, '', 0, 0).subscribe(
+            (prod) => {
+                filtered = prod.productos ?? [];
+                this.sugerencias = filtered;
+            },
+            (err) => {
+                // filtered = [];
+            }
+        );
+    }
+    pagar(){
+        
+    }
 }
